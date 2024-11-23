b1c5200d5911643489d863f491e751e2aaa373ea
Ditofry <bbarrett@victorops.com>
2024-11-22 23:54:09 -0700

    Added disclaimer about js retrieval code

--- util.js
+++ util.js
@@ -1,3 +1,10 @@
+// This can be used to pull the css from a given page.
+// It it meant to be copied and run, verbatim, in browser console
+// I tried to just pull CSS from the dom/sources, but css for
+// this site is injected at runtime and therefore must be
+// pull from script tags programatically.
+// I got the idea from https://developer.chrome.com/docs/css-ui/css-in-js
+
 const cssString = "";
 const styleTags = Array.from(document.getElementsByTagName("style"));
 
